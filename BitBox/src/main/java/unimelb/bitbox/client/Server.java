688aba92692717053d15809e26dd76012e5ffecf
Eleanor McMurtry <elmcmurtry1@gmail.com>
2019-05-26 16:21:46 +1000

    made server multithreaded

--- BitBox/src/main/java/unimelb/bitbox/client/Server.java
+++ BitBox/src/main/java/unimelb/bitbox/client/Server.java
@@ -23,7 +23,6 @@ public class Server implements Runnable {
     private static final String authorized_keys = Configuration.getConfigurationValue("authorized_keys");
     private final ArrayList<SSHPublicKey> keys = new ArrayList<>();
     private SecretKey key;
-    private boolean authenticated;
     private ServerMain server;
 
     public Server(ServerMain server) {
@@ -39,31 +38,35 @@ public class Server implements Runnable {
         }
     }
 
+    private void handleClient(ClientData client) {
+        Socket socket = client.getSocket();
+        ServerMain.log.info(client + ": received connection");
+
+        // Open the read/write streams and process messages until the socket closes
+        try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                // Read a message, and pass it to the handler
+                try {
+                    out.write(handleMessage(message, client).toJson() + "\n");
+                    out.flush();
+                } catch (ResponseFormatException e) {
+                    ServerMain.log.warning(client + ": malformed message: " + e.getMessage());
+                }
+            }
+        } catch (IOException | CryptoException e) {
+            ServerMain.log.warning(client + ": error while responding: " + e.getMessage());
+        }
+    }
+
     @Override
     public void run() {
         // Accept connections repeatedly.
         try (ServerSocket serverSocket = new ServerSocket(clientPort)) {
             while (!serverSocket.isClosed()) {
-                authenticated = false;
                 Socket socket = serverSocket.accept();
-                ServerMain.log.info("Received client connection from " + socket.getInetAddress().toString() + ":" + socket.getPort());
-
-                // Open the read/write streams and process messages until the socket closes
-                try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    String message;
-                    while ((message = in.readLine()) != null) {
-                        // Read a message, and pass it to the handler
-                            try {
-                                out.write(handleMessage(message).toJson() + "\n");
-                                out.flush();
-                            } catch (ResponseFormatException e) {
-                                ServerMain.log.warning("malformed message: " + e.getMessage());
-                            }
-                    }
-                } catch (IOException | CryptoException e) {
-                    ServerMain.log.warning("Error while responding to client: " + e.getMessage());
-                }
+                new Thread(() -> handleClient(new ClientData(socket))).start();
             }
         } catch (IOException e) {
             ServerMain.log.severe("Error with server socket:");
@@ -76,14 +79,14 @@ public class Server implements Runnable {
         }
     }
 
-    private JsonDocument handleMessage(String message)
+    private JsonDocument handleMessage(String message, ClientData client)
             throws CryptoException, ResponseFormatException {
         // Parse the message. If there is a payload key, then we need to decrypt the payload to get the actual message
         JsonDocument document = JsonDocument.parse(message);
         if (document.containsKey("payload")) {
             document = Crypto.decryptMessage(key, document);
         }
-        ServerMain.log.info("Received from client: " + document.toJson());
+        ServerMain.log.info(client + ": received " + document.toJson());
 
         // Generate a response
         JsonDocument response = new JsonDocument();
@@ -93,6 +96,7 @@ public class Server implements Runnable {
         // Auth requests need to be handled separately because they rely on key data
         if (command.equals("AUTH_REQUEST")) {
             String ident = document.require("identity");
+            client.setIdent(ident);
             response.append("command", "AUTH_RESPONSE");
 
             // Look up the provided ident in our list of keys to find the relevant key
@@ -102,24 +106,24 @@ public class Server implements Runnable {
                     .findFirst();
             if (matchedKey.isPresent()) {
                 try {
-                    ServerMain.log.info("Generating new session key for " + ident);
+                    ServerMain.log.info(ident + ": generating new session key");
                     // We attempt to generate a key, and then encrypt it with the looked-up public key
                     key = Crypto.generateSecretKey();
-                    ServerMain.log.info("Generated session key: " + new String(Base64.getEncoder().encode(key.getEncoded())));
+                    ServerMain.log.info(ident + ": generated session key " + new String(Base64.getEncoder().encode(key.getEncoded())));
                     String encryptedKey = Crypto.encryptSecretKey(key, matchedKey.get().getKey());
                     response.append("AES128", encryptedKey);
-                    ServerMain.log.info("Encrypted session key: " + encryptedKey);
+                    ServerMain.log.info(ident + ": encrypted session key " + encryptedKey);
                     response.append("status", true);
                     response.append("message", "public key found");
                 } catch (CryptoException e) {
                     // In case the crypto algorithms failed, we send a failure response
                     e.printStackTrace();
-                    ServerMain.log.warning("Failed encryption: " + e.getMessage() + ": " + ident);
+                    ServerMain.log.warning(ident + ": failed encryption: " + e.getMessage());
                     response.append("status", false);
                     response.append("message", "error generating key: " + e.toString());
                 }
             } else {
-                ServerMain.log.warning("Client provided unknown ident: " + ident);
+                ServerMain.log.warning(ident + ": unknown ident");
                 // If the ident wasn't found, inform the user
                 response.append("status", false);
                 response.append("message", "public key not found");
@@ -128,12 +132,65 @@ public class Server implements Runnable {
             response = ClientResponse.getResponse(command, server, document);
         }
 
-        ServerMain.log.info("Sending client: " + response.toJson());
-        if (authenticated) {
+        ServerMain.log.info(client + ": sending " + response.toJson());
+        if (client.isAuthenticated()) {
             response = Crypto.encryptMessage(key, response);
         }
-        ServerMain.log.info("Response encrypted");
-        authenticated = true;
+        ServerMain.log.info(client + ": response encrypted");
+        client.authenticate();
         return response;
+    }
+}
+
+class ClientData {
+    private Socket socket;
+    private boolean authenticated;
+    private String ident;
+
+    private boolean anonymous = true;
+
+    public ClientData(Socket socket) {
+        this.socket = socket;
+        ident = socket.getInetAddress().toString() + ":" + socket.getPort();
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setIdent(String ident) {
+        if (anonymous) {
+            this.ident = ident;
+            anonymous = false;
+        }
+    }
+
+    public void authenticate() {
+        if (!anonymous) {
+            authenticated = true;
+        }
+    }
+
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public String getIdent() {
+        return ident;
+    }
+
+    @Override
+    public String toString() {
+        return ident + ": " + (authenticated ? "authenticated" : "unauthenticated");
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        return rhs instanceof ClientData && rhs.toString().equals(toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
     }
 }
